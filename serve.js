03579f13c43d36c876bb97e991d04550d237e26e
jhonceth <mrpiano16@gmail.com>
2024-06-06 09:35:44 -0400

    deplo v1

---
+++ serve.js
@@ -0,0 +1,107 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+// Configuración del servidor Express
+var app = express();
+var PORT = 3000;
+app.use(express.json());
+app.post('/receiveData', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, user, frameData, _b, fid_1, hash, neynarUrl, options, response, json, neynarData, reactions, successObject_1, error_1;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0:
+                _c.trys.push([0, 3, , 4]);
+                _a = req.body, user = _a.user, frameData = _a.frameData;
+                // Verificación inicial para asegurarse de que user y frameData existen
+                if (!user || !frameData) {
+                    console.error('Datos del usuario y frameData son necesarios');
+                    return [2 /*return*/, res.status(400).send('Datos del usuario y frameData son necesarios')];
+                }
+                _b = frameData.castId, fid_1 = _b.fid, hash = _b.hash;
+                if (!fid_1 || !hash) {
+                    console.error('Datos de fid y hash son necesarios');
+                    return [2 /*return*/, res.status(400).send('Datos de fid y hash son necesarios')];
+                }
+                // Imprimir datos de frameData y del usuario (opcional)
+                console.log('fid:', fid_1);
+                console.log('hash:', hash);
+                console.log('Usuario:', user.username);
+                console.log('Nombre de pantalla:', user.displayName);
+                console.log('URL de PFP:', user.pfp.url);
+                console.log('Biografía:', user.profile.bio.text);
+                neynarUrl = "https://api.neynar.com/v2/farcaster/reactions/cast?hash=".concat(hash, "&types=recast&limit=25");
+                options = {
+                    method: 'GET',
+                    headers: { accept: 'application/json', api_key: '30A5C95E-44B4-4581-B76C-027B4D5C2469' }
+                };
+                return [4 /*yield*/, fetch(neynarUrl, options)];
+            case 1:
+                response = _c.sent();
+                return [4 /*yield*/, response.json()];
+            case 2:
+                json = _c.sent();
+                // Validar que la respuesta JSON tiene la forma esperada
+                if (typeof json === 'object' && json !== null && Array.isArray(json.reactions) && 'cursor' in json) {
+                    neynarData = json;
+                    reactions = neynarData.reactions;
+                    successObject_1 = { success: false };
+                    reactions.forEach(function (reaction) {
+                        if (reaction.reaction_type === 'recast' && reaction.user.object === 'user' && reaction.user.fid === fid_1) {
+                            successObject_1 = { success: true }; // Cambiamos el éxito a verdadero si se cumple la condición
+                        }
+                    });
+                    console.log(successObject_1); // Devolvemos el objeto de éxito
+                    res.json(successObject_1); // Enviar la respuesta al cliente
+                }
+                else {
+                    throw new Error('Respuesta de la API de Neynar no tiene el formato esperado');
+                }
+                return [3 /*break*/, 4];
+            case 3:
+                error_1 = _c.sent();
+                console.error('Error:', error_1);
+                res.status(500).send('Error al procesar los datos recibidos');
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
+app.listen(PORT, function () {
+    console.log("Servidor escuchando en el puerto ".concat(PORT));
+});
